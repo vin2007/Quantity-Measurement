309fa73c13f8d335b7d12ff1cdf8ccfbecb08e50
vin2007 <vineshreddy2006@gmail.com>
2026-04-24 15:22:26 +0530

    UC1

--- App/src/Test/UC1Test.java
+++ App/src/Test/UC1Test.java
@@ -1,4 +1,35 @@
-package Test;
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.apps.quantitymeasurement.UC1.Feet;
 
 public class UC1Test {
+
+    @Test
+    void testEquality_SameValue() {
+        assertEquals(new Feet(1.0), new Feet(1.0));
+    }
+
+    @Test
+    void testEquality_DifferentValue() {
+        assertNotEquals(new Feet(1.0), new Feet(2.0));
+    }
+
+    @Test
+    void testEquality_NullComparison() {
+        assertNotEquals(new Feet(1.0), null);
+    }
+
+    @Test
+    void testEquality_DifferentClass() {
+        assertNotEquals(new Feet(1.0), "1.0");
+    }
+
+    @Test
+    void testEquality_SameReference() {
+        Feet f = new Feet(1.0);
+        assertEquals(f, f);
+    }
 }
--- App/src/main/UC1.java
+++ App/src/main/UC1.java
@@ -1,4 +1,23 @@
-package main;
+package com.apps.quantitymeasurement;
 
 public class UC1 {
+
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            if (Double.isNaN(value)) {
+                throw new IllegalArgumentException();
+            }
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
+    }
 }
